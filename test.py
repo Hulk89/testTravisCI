1dba5e6a9c659b46559ba286a7d8198560716445
hyeongseok oh <hyeongseokoh@hyeongseokui-MacBook.local>
2018-02-17 22:19:14 +0900

    What if I wrong?

--- test.py
+++ test.py
@@ -15,10 +15,13 @@ class ModuleTest1(unittest.TestCase):
         
     def testsum2(self):
         self.assertEqual(sum(1,-1),0)
+
+    def whatIfNotGood(self):
+        self.assertEqual(sum(1,-1),10)
  
 if __name__ == '__main__':
     testSuite = unittest.TestSuite()
-    for testmethod in ('testsum1', 'testsum2'):
+    for testmethod in ('testsum1', 'testsum2', 'whatIfNotGood'):
         testSuite.addTest(ModuleTest1(testmethod))
     runner = unittest.TextTestRunner()
     runner.run(testSuite)
